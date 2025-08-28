bcd2e2cf3f009d1e49bf32f8f236365c7d877061
erafaziramohd28 <erafaziramohd28@gmail.com>
2025-08-28 21:02:12 +0800

    Add generateStaticParams to all dynamic routes for static export compatibility

--- src/app/services/[id]/page.tsx
+++ src/app/services/[id]/page.tsx
@@ -1,11 +1,10 @@
-'use client';
-
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'next/navigation';
 import ServiceDetailContent from './ServiceDetailContent';
 
+export async function generateStaticParams() {
+  // Return empty array for static export - pages will be generated on demand
+  return [];
+}
+
 export default function ServiceDetailPage() {
-  const params = useParams();
-  const id = params?.id as string;
-  return <ServiceDetailContent serviceId={id} />;
+  return <ServiceDetailContent />;
 }
